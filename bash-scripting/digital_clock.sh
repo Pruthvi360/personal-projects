56eadd4b10b467b87b4b74c8e4d6947736594b18
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-13 20:56:22 +0530

    Create digital_clock.sh

---
+++ bash-scripting/digital_clock.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+clear
+#set -x
+<< mycom
+echo "
+###################################################################
+#                                                                 #
+# AUTHOR: PRUTHVI S                                               #
+# DATE: 07-04-23                                                  #
+# PURPOSE: BASE SCRIPTING                                         #
+#                                                                 #
+###################################################################
+"
+mycom
+
+while :
+do
+  echo "\033[92m $(date '+%T)"
+  sleep 1
+  clear
+done
